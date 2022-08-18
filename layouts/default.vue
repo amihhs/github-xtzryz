b11beca0ea6fa56e5fa0ff82d3025eea2b0bcfa2
张建华 <1804728126@qq.com>
2022-08-18 15:58:23 +0800

    use component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,5 @@
-<script setup lang='ts'>
-import { isClient, useWindowScroll } from '@vueuse/core'
-const isVisibleHeader = ref(false)
-if (isClient) {
-  const { y } = useWindowScroll()
-  watch(y, (newValue, oldValue) => {
-    if (newValue > oldValue && newValue > 200)
-      isVisibleHeader.value = true
-    else if (newValue < oldValue)
-      isVisibleHeader.value = false
-  })
-}
-</script>
-
 <template>
-  <div
-    class="default-header transition"
-    :class="{ 'header-hidden': isVisibleHeader }"
-  >
-    <div flex items-center justify-between m-auto px-5 h-full max="w-1440px">
-      <nuxt-link to="/">
-        <div class="logo text-8 font-bold" sm="text-10">
-          LOGO
-        </div>
-      </nuxt-link>
-      <div class="setting">
-        aa
-      </div>
-    </div>
-  </div>
-  <div
-    class="default-header-place"
-    :class="{ 'header-hidden': isVisibleHeader }"
-  />
+  <Nav />
   <main class="py-20 px-10 text-center">
     <slot />
     <Footer />
@@ -40,17 +8,3 @@ if (isClient) {
     </div>
   </main>
 </template>
-
-<style lang='scss' scoped>
-.default-header {
-  @apply fixed w-full top-0 z-999 shadow-lg backdrop-blur-md;
-  @apply bg-white bg-opacity-60 dark:bg-dark-800;
-  @apply h-15 sm:h-20;
-}
-.default-header-place {
-  @apply w-full top-0 z-1 h-15 sm:h-20;
-}
-.header-hidden{
-  transform: translate3d(0,-100%,0);
-}
-</style>
