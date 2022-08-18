c9b5df9bca643d08787a94c4ed95388b9c439fd3
张建华 <1804728126@qq.com>
2022-08-18 18:29:44 +0800

    change name

---
+++ components/ISSUE.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="ISSUE transition"
+  />
+  <div
+    class="default-header-place"
+  />
+</template>
+
+<style lang='scss' scoped>
+.ISSUE {
+  @apply fixed w-full top-0 z-999 shadow-lg backdrop-blur-md bg-white bg-opacity-60 dark:bg-dark-800 h-15 sm:h-20;
+}
+
+.default-header-place {
+  @apply w-full top-0 z-1 h-15 sm:h-20;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <Nav />
+  <ISSUE />
   <main class="py-20 px-10 text-center">
     <slot />
     <Footer />
