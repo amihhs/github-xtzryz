66ff004326a3c436fd69fcd8b5d5405b9f553f6f
张建华 <1804728126@qq.com>
2022-08-18 18:35:01 +0800

    Simplify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,3 @@
 <template>
   <ISSUE />
-  <main class="py-20 px-10 text-center">
-    <slot />
-    <Footer />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Default Layout]
-    </div>
-  </main>
 </template>
